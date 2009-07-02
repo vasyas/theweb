4a6e4513b5e0d48cc1b84ac75562e6c37207aa99
vasyl.stashuk <vasyl.stashuk@52d64424-08d1-11de-b753-7319d013a8fa>
2009-07-02 16:25:37 +0000

    api slightly improved

--- trunk/src/java/theweb/VelocityTemplateMarkup.java
+++ trunk/src/java/theweb/VelocityTemplateMarkup.java
@@ -8,13 +8,24 @@ import theweb.velocity.VelocityTemplate;
 public class VelocityTemplateMarkup implements Markup {
     
     private VelocityTemplate velocityTemplate;
-    private Map<String, Object> context = new HashMap<String, Object>();
+    public Map<String, Object> context;
 
     public VelocityTemplateMarkup(Class<?> base, String template, Object ... context) {
-        this.velocityTemplate = new VelocityTemplate(base, template);
+        this(base, template, toMap(context));
+    }
+    
+    private static Map<String, Object> toMap(Object[] arr) {
+        Map<String, Object> context = new HashMap<String, Object>();
         
-        for (int i = 0; i < context.length; i = i + 2)
-            this.context.put(context[i].toString(), context[i + 1]);
+        for (int i = 0; i < arr.length; i = i + 2)
+            context.put(arr[i].toString(), arr[i + 1]);
+        
+        return context;
+    }
+
+    public VelocityTemplateMarkup(Class<?> base, String template, Map<String, Object> context) {
+        this.velocityTemplate = new VelocityTemplate(base, template);
+        this.context = context;
     }
 
     @Override
