53c99d230fb3e1abd4b9618f4fde7d5f76af1a52
vasyl.stashuk <vasyl.stashuk@52d64424-08d1-11de-b753-7319d013a8fa>
2009-08-10 08:24:21 +0000

    removed Renderer class

--- trunk/src/java/theweb/RenderOutcome.java
+++ trunk/src/java/theweb/RenderOutcome.java
@@ -1,5 +1,7 @@
 package theweb;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -8,10 +10,20 @@ public class RenderOutcome implements Outcome {
     @Override
     public void process(Page page, HttpServletRequest request, HttpServletResponse response) throws Exception {
         try {
-            new Renderer().render(page, response);
+            Markup markup = page.markup();
+            
+            response.setContentType("text/html; charset=UTF-8");
+            response.addHeader("Pragma", "no-cache");
+            response.addHeader("Expires", "0");
+            response.addHeader("Etag", "" + Math.random());
+
+            PrintWriter writer = response.getWriter();
+
+            writer.write(markup.render());
+            
+            writer.flush();
         } finally {
             Messages.get().clear();
         }
     }
-
 }
