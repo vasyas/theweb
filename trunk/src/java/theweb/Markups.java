2401fd69c13ad4c09543abc5e946605ba7145c3f
vasyl.stashuk <vasyl.stashuk@52d64424-08d1-11de-b753-7319d013a8fa>
2012-09-28 10:50:49 +0000

    helpers

--- trunk/src/java/theweb/Markups.java
+++ trunk/src/java/theweb/Markups.java
@@ -3,17 +3,27 @@ package theweb;
 public class Markups implements Markup {
 
     private final Markup[] markups;
+    private String separator = ""; 
 
+    public Markups(String separator, Markup ... markups) {
+    	this.separator = separator;
+    	this.markups = markups;
+    }
+    
     public Markups(Markup ... markups) {
-        this.markups = markups;
+    	this("", markups);
     }
     
     @Override
     public String render() {
         StringBuilder s = new StringBuilder();
         
-        for (Markup markup : markups)
+        for (Markup markup : markups) {
+        	if (s.length() > 0)
+        		s.append(separator);
+        	
             s.append(markup.render());
+        }
         
         return s.toString();
     }
