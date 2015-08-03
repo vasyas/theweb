14e5d4516a5e6b5b99f45e9354af3ce502ea7cea
vasyas <vasyl@stashuk.com>
2015-08-03 14:29:19 +0300

    firefox fixes

--- src/java/theweb/NoResponse.java
+++ src/java/theweb/NoResponse.java
@@ -1,10 +1,16 @@
 package theweb;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class NoResponse implements Response {
     @Override
     public void send(Page page, HttpExchange exchange) throws IOException {
-    	exchange.getWriter().close();
+        // firefox requires empty response to be written!
+        exchange.setContentType("text/html");
+
+        PrintWriter writer = exchange.getWriter();
+        writer.write("");
+    	writer.close();
     }
 }
